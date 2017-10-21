34373fcee29e5dc6de83a476820636bc3916b8d7
hongmk <isentator@naver.com>
2017-10-21 16:28:57 +0900

    sql일붑수정

--- sql/create_user_table.sql
+++ sql/create_user_table.sql
@@ -1,24 +1,10 @@
-create database restful;
-
 use restful;
 
 drop table user;
 
 CREATE TABLE user (
-    row_id INTEGER NOT NULL AUTO_INCREMENT,
-    user_id VARCHAR(50) NOT NULL,
-    nicname VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    alert_recive_yn TINYINT,
-    app_token TEXT,
-    signup_date DATETIME DEFAULT NOW(),
-    last_modify_date DATETIME,
-    withdraw_date DATETIME,
-    PRIMARY KEY (row_id , user_id , nicname)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name text,
+    age integer,
+    PRIMARY KEY (id)
 );
-
-select * from user;
-
-SELECT * 
-  FROM USER_CONSTRAINTS 
- WHERE TABLE_NAME = 'user';
