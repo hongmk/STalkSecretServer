a99852a1261ebd4f5b7e006429dcdacfcf0dd163
hongmk <isentator@naver.com>
2017-10-27 17:25:21 +0900

    sql 추가

--- sql/create_user_table.sql
+++ sql/create_user_table.sql
@@ -2,9 +2,27 @@ use restful;
 
 drop table user;
 
-CREATE TABLE user (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    name text,
-    age integer,
-    PRIMARY KEY (id)
+create table user(
+id integer primary key auto_increment,
+user_id text,
+password text,
+name text,
+age integer
 );
+
+select * from user;
+select * from user where user_id is null  ;
+
+delete from user ;
+
+
+create table user_login(
+id integer primary key auto_increment,
+user_real_id integer,
+token text,
+created_at DATETIME default current_timestamp
+);
+
+select * from user_login;
+
+delete from user_login where id ='';
